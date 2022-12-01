6f68a07566850a957504ea77329c4078a4cd5902
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-11-30 20:45:45 -0500

    Updating 6-final-user.js file Write and export a function named handleProfileSignup. It should accept three arguments firstName (string), lastName (string), and fileName (string)

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
 
-  return Promise.all([promise1, promise2]).then((values) => {
+  return Promise.allSettled([promise1, promise2]).then((values) => {
     const result = [];
     values.forEach((element) => {
       if (element.status === 'fulfilled') {
