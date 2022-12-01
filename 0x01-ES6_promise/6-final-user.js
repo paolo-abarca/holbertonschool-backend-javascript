cbb5ed53ba7cdfcae5ae799f546977ee52c2ce00
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-11-30 20:43:53 -0500

    Adding 6-final-user.js file Write and export a function named handleProfileSignup. It should accept three arguments firstName (string), lastName (string), and fileName (string)

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+
+  return Promise.all([promise1, promise2]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
+}
