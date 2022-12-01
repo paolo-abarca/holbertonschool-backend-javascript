306ccc798ea1d7626958e5c2c73fd4bcb0eb9865
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-11-30 19:34:29 -0500

    Adding 3-all.js file Knowing that the functions in utils.js return promises, use the prototype below to collectively resolve all promises and log body firstName lastName to the console

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  Promise.all([promise1, promise2]).then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
+
+  return Promise;
+}
