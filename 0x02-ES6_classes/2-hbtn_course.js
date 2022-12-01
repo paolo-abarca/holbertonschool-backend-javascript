1be14be053b07416737137d2a9ff5e8b3b5f889f
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-01 14:44:14 -0500

    Adding 2-hbtn_course.js file Implement a class named HolbertonCourse

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    if (typeof n !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = n;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(n) {
+    if (typeof n !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = n;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(n) {
+    this._students = n;
+  }
+}
