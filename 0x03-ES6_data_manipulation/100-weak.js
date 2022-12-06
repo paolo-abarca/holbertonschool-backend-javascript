2a30c3a78883627ef714d31d63e7c70cca198e7b
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-06 17:14:04 -0500

    Updating 100-weak.js file Export a const instance of WeakMap and name it weakMap, Export a new function named queryAPI

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,10 +4,11 @@ export function queryAPI(endpoint) {
   if (weakMap.get(endpoint) === undefined) {
     weakMap.set(endpoint, 1);
   } else {
+    const iterator = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, iterator);
+
     if (weakMap.get(endpoint) >= 5) {
       throw new Error('Endpoint load is high');
     }
-    const iterator = weakMap.get(endpoint) + 1;
-    weakMap.set(endpoint, iterator);
   }
 }
