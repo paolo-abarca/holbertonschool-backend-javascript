d779624725ee2e9cbb30c05b65800e7998ad1019
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-07 19:17:02 -0500

    Adding main.ts file Write an interface named Directors that extends Teacher. It requires an attribute named numberOfReports(number)

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,5 +14,18 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
-
 console.log(teacher3);
+
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
