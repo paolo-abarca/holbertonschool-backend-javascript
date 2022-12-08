06e4d2da0489ff5d8803d51d114cf19fdd87fa4d
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-07 23:20:06 -0500

    Adding main.ts file Write a String literal type named Subjects allowing a variable to have the value Math or History only. Write a function named teachClass

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -65,3 +65,17 @@ function executeWork(employee: Director | Teacher): string {
 
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+
+
+// Task #7
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  return 'Teaching History';
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
