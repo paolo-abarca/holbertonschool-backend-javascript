41d10564355728b29f09773ee91d26f665b141b5
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-07 19:32:57 -0500

    Adding main.ts file Write a function printTeacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -29,3 +29,13 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`
+}
+console.log(printTeacher("John", "Doe"));
