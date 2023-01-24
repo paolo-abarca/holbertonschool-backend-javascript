6041bc04b9da456b4efd6b00bc779d0ce2d729d4
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-01-24 00:15:43 -0500

    Updating 1-stdin.js file Create a program named 1-stdin.js that will be executed through command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  console.log(`Your name is: ${name}`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.resume();
-
 process.stdin.on('end', () => {
-  console.log('This important software is now closing')
+  process.stdout.write('This important software is now closing\n');
 });
