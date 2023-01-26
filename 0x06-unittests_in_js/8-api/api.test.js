113b8f66fe7f44a01872a56d635c25a62ea162c7
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-01-26 00:28:21 -0500

    Adding api.test.js file test for task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,31 @@
-const request = require('request');
-const chai = require("chai");
-const expect = chai.expect;
-
-describe('Index page', function () {
-  it('...', function (done) {
-    request('http://localhost:7865', (err, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
-      done()
-    })
-  })
-});
+const {
+    expect,
+  } = require('chai');
+  const request = require('request');
+  
+  describe('IndexPage', () => {
+    it('should have the correct status code', () => {
+      request('http://localhost:7865', (_error, response, _body) => {
+        expect(response.statusCode).to.equal(200);
+      });
+    });
+  
+    it('should have the correct result', () => {
+      request('http://localhost:7865', (_err, _res, body) => {
+        expect(body).to.contain('Welcome to the payment system');
+      });
+    });
+  
+    it('should have the correct content type', () => {
+      request('http://localhost:7865', (_err, res, _body) => {
+        expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
+      });
+    });
+  
+    it('should have the correct content length', () => {
+      request('http://localhost:7865', (_err, res, _body) => {
+        expect(res.headers['content-length']).to.equal('29');
+      });
+    });
+  
+  });
