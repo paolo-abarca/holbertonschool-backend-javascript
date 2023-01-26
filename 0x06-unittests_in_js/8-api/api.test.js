4126563106bb658160d148bdca7266bd70577513
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-01-26 00:17:38 -0500

    Adding 8-api directory  contains package.json, api.js and api.test.js

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const chai = require("chai");
+const expect = chai.expect;
+
+describe('Index page', function () {
+  it('...', function (done) {
+    request('http://localhost:7865', (err, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done()
+    })
+  })
+});
