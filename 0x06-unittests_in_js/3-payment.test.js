630edd5e848970d0f1e5c0a6a4c6bc499acded51
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-01-26 00:09:08 -0500

    Adding 3-payment.test.js file test for task 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sendPaymentRequestToApi = require('./3-payment')
+const Utils = require('./utils')
+const sinon = require('sinon')
+const chai = require("chai");
+const expect = chai.expect;
+
+describe('payment', function () {
+  it('...', function () {
+    const callback = sinon.spy(Utils, 'calculateNumber')
+    sendPaymentRequestToApi(100, 20);
+    expect(callback.calledOnce).to.be.true;
+    expect(callback.calledWith('SUM', 100, 20)).to.be.true;
+    callback.restore()
+  })
+})
